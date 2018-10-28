3c05d58823855103956bcb409ce70d882fa49f21
Andy Waite <andyw8@users.noreply.github.com>
2018-10-28 12:38:18 -0400

    Add Components exercise

---
+++ ex-components/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <my-header />
+        <hr>
+        <div class="row">
+          <my-servers />
+          <my-server-details />
+        </div>
+        <hr>
+        <div class="row">
+            <my-footer />
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+import Servers from './Servers.vue'
+import ServerDetails from './ServerDetails.vue'
+
+export default {
+  components: {
+    'my-header': Header,
+    'my-footer': Footer,
+    'my-servers': Servers,
+    'my-server-details': ServerDetails
+  }
+}
+
+</script>
+
+
+<style>
+
+</style>
