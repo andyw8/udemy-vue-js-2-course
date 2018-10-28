c87b3752aa0370ea600e03df0dd554d1e38a5f38
Andy Waite <andyw8@users.noreply.github.com>
2018-10-28 18:16:55 -0400

    Ex7

---
+++ ex7/src/components/Server/Server.vue
@@ -0,0 +1,24 @@
+<template>
+  <li class="list-group-item">
+    <div @click="showDetails">
+      Server #{{ server.id }}
+    </div>
+  </li>
+</template>
+
+<script>
+  import { eventBus } from '../../main';
+
+  export default {
+    props: ['server'],
+    methods: {
+      showDetails: function() {
+        eventBus.$emit("showDetailsClicked", this.server);
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ ex7/src/components/Server/Servers.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <app-server
+              v-for="server in servers"
+              :server="server" />
+        </ul>
+    </div>
+</template>
+
+<script>
+    import Server from './Server.vue';
+
+    export default {
+        components: {
+            'app-server': Server
+        },
+        data: function() {
+          return {
+            servers: [
+              { id: 1, status: "Normal" },
+              { id: 2, status: "Degraded" },
+              { id: 3, status: "Error" },
+              { id: 4, status: "Failed" },
+              { id: 5, status: "Unknown" }
+            ]
+          }
+        }
+    }
+</script>
+
+<style>
+
+</style>
