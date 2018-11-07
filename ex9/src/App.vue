217aa2045f35cdd4acc8e0f658d192ce55705014
Andy Waite <andyw8@users.noreply.github.com>
2018-11-06 20:53:30 -0500

    Ex9 first parts

--- ex9/src/App.vue
+++ ex9/src/App.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="container">
-        <form>
+        <form v-if="!submitted">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <!-- Exercise 1 -->
-                    <!-- Create a Signup Form where you retrieve the following Information -->
-                    <!-- Full Name (First Name + Last Name) -->
-                    <!-- Mail -->
-                    <!-- Password -->
-                    <!-- Store Data? Yes/No -->
+                    <p>First Name: <input v-model="firstName" type="text"></p>
+                    <p>Last Name: <input v-model="lastName" type="text"></p>
+                    <p>Mail: <input v-model="mail" type="text" /></p>
+                    <p>Password: <input v-model="password" type="password" /></p>
+                    <p>Store in Database?:
+                        <label>
+                            <input type="radio" v-model="store" value="yes" id="store-yes" name="store"> Yes
+                        </label>
 
-                    <!-- Exercise 2 -->
-                    <!-- Only display the Form if it has NOT been submitted -->
-                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
+                        <label>
+                            <input type="radio" v-model="store" value="no" id="store-no" name="store"> No
+                        </label>
+                    </p>
+                    <input @click.prevent="stuff" type="submit" />
                     <!-- Exercise 3 -->
                     <!-- Edit the Example from above and create a custom "Full Name" Control -->
                     <!-- which still holds the First Name and Last Name Input Field -->
@@ -22,16 +25,16 @@
         </form>
         <hr>
         <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <div v-if="submitted" class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ fullName }}</p>
+                        <p>Mail: {{ mail }}</p>
+                        <p>Password: {{ password }}</p>
+                        <p>Store in Database?: {{ store }}</p>
                     </div>
                 </div>
             </div>
@@ -41,6 +44,27 @@
 
 <script>
     export default {
+      data: function() {
+        return {
+          submitted: false,
+          firstName: "",
+          lastName: "",
+          mail: "",
+          password: "",
+          store: "yes"
+        };
+      },
+      computed: {
+        fullName: function() {
+          return this.firstName + ' ' + this.lastName;
+        },
+      },
+      methods: {
+        stuff: function(event) {
+          this.submitted = true;
+          console.log("stuff");
+        }
+      }
     }
 </script>
 
